d59334d61288fb2d32413c4b99eeb12f18e1ddf9
John Hilley <JHilley@ppsconline.local>
2017-03-21 15:31:21 -0400

    updated database config

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': '192.168.1.51',
+        'port': '4300',
+        'user': 'remoteRetailer',
+        'password': 'Ppsc3375!'
+    },
+	'database': 'testing',
+    'users_table': 'users'
+};
